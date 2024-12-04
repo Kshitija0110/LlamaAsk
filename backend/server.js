6257eb54edd6e9f99b90e38f0422c11206db57e5
Kshitija <kshitija.22210574@viit.ac.in>
2024-12-04 22:31:18 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(express.static(path.join(__dirname, '../frontend/public')));
 
 const mongoURI = process.env.MONGODB_URI;
 
