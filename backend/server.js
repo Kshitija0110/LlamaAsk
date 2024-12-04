5c2811a67b7d03b820d0009408417b9d3f15b234
Kshitija <kshitija.22210574@viit.ac.in>
2024-12-04 23:13:46 +0530

    fifth commit

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4000;
 
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../frontend/public')));
+app.use('/src',express.static(path.join(__dirname, '../frontend/src')));
 
 const mongoURI = process.env.MONGODB_URI;
 
