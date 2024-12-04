58122b001f5b00632fb35d1e0bb662d9be426034
Kshitija <kshitija.22210574@viit.ac.in>
2024-12-04 22:55:30 +0530

    fourth commit

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware to parse JSON requests
 app.use(bodyParser.json());
